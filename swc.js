198531ca051e8e5316e53d3f36313867d823a560
Guy Bedford <guybedford@gmail.com>
2022-02-18 16:48:03 -0800

    0.1.4

--- swc.js
+++ swc.js
@@ -53,7 +53,8 @@ Chomp.registerTemplate('swc', function ({
     targets,
     deps: [...deps, ...!swcRc || ENV.CHOMP_EJECT ? [] : ['.swcrc'], ...ENV.CHOMP_EJECT ? [] : ['node_modules/@swc/core', 'node_modules/@swc/cli']],
     env,
-    display: 'stderr-only',
+    stdio: 'stderr-only',
+    display,
     run: `node ./node_modules/@swc/cli/bin/swc.js $DEP -o $TARGET${
         !swcRc ? ' --no-swcrc' : ''
       }${
