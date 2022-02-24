47ed051ce4d8b7914a329713e06cb58dcdce0433
Guy Bedford <guybedford@gmail.com>
2022-02-23 16:33:18 -0800

    npm magic

--- npm.js
+++ npm.js
@@ -1,20 +1,26 @@
-
 if (ENV.PACKAGE_MANAGER !== 'yarn' && ENV.PACKAGE_MANAGER !== 'pnpm')
   ENV.PACKAGE_MANAGER = 'npm';
 
 Chomp.registerTask({
   name: 'npm:install',
   display: 'init-only',
-  targets: [{ npm: 'package-lock.json', yarn: 'yarn.lock', pnpm: 'pnpm-lock.yaml' }[ENV.PACKAGE_MANAGER], 'node_modules'],
+  targets: ['node_modules', { npm: 'package-lock.json', yarn: 'yarn.lock', pnpm: 'pnpm-lock.yaml' }[ENV.PACKAGE_MANAGER]],
   dep: 'package.json',
   run: `${ENV.PACKAGE_MANAGER} install`
 });
 
+const NPM_MISSING_MESSAGE = "\n\x1b[93mChomp\x1b[0m: Some packages are missing.";
+
 Chomp.registerTemplate('npm', function ({ name, deps, env, display, templateOptions: { packages, dev, autoInstall, ...invalid } }) {
+  const isWin = ENV.PATH.match(/\\|\//)[0] !== '/';
   if (Object.keys(invalid).length)
     throw new Error(`Invalid npm template option "${Object.keys(invalid)[0]}"`);
   if (!packages)
     throw new Error('npm template requires the "packages" option to be a list of packages to install.');
+  const nodeModulesTargets = packages.map(pkg => {
+    const versionIndex = pkg.indexOf('@', 1);
+    return `node_modules/${versionIndex === -1 ? pkg : pkg.slice(0, versionIndex)}`;
+  });
   return ENV.CHOMP_EJECT ? [] : autoInstall ? [{
     name,
     display,
@@ -29,7 +35,7 @@ Chomp.registerTemplate('npm', function ({ name, deps, env, display, templateOpti
       target: `node_modules/${versionIndex === -1 ? pkg : pkg.slice(0, versionIndex)}`,
       invalidation: 'not-found',
       display: 'none',
-      dep: 'package.json',
+      deps: ['npm:install'],
       env,
       run: `${ENV.PACKAGE_MANAGER} install ${packages.join(' ')}${dev ? ' -D' : ''}`
     };
@@ -38,11 +44,11 @@ Chomp.registerTemplate('npm', function ({ name, deps, env, display, templateOpti
     env,
     invalidation: 'not-found',
     display: 'none',
-    targets: packages.map(pkg => {
-      const versionIndex = pkg.indexOf('@', 1);
-      return `node_modules/${versionIndex === -1 ? pkg : pkg.slice(0, versionIndex)}`;
-    }),
-    run: `echo "\n\x1b[93mChomp\x1b[0m: Some packages are missing. Run \x1b[1m${packageManager} install ${packages.join(' ')}${dev ? ' -D' : ''}\x1b[0m, or add \x1b[36mauto-install = true\x1b[0m to the npm template configuration.\n"`
+    targets: nodeModulesTargets,
+    deps: ['npm:install'],
+    run: (isWin ? 'If (' + nodeModulesTargets.map(t => `(Test-Path -Path "${t}")`).join(' -And ') + ') { } Else { ' : nodeModulesTargets.map(t => `[ ! -d "${t}" ] || `).join('')) + 
+      `echo "${NPM_MISSING_MESSAGE} Run \x1b[1m${ENV.PACKAGE_MANAGER} install ${packages.join(' ')}${dev ? ' -D' : ''}\x1b[0m, or add \x1b[36mauto-install = true\x1b[0m to the npm template configuration.\n"` +
+      (isWin ? '\n}' : '')
   }];
 });
 
@@ -65,19 +71,19 @@ Chomp.registerBatcher('npm', function (batch, running) {
   if (running.length >= ENV.CHOMP_POOL_SIZE) return;
   const defer = [], completionMap = {};
   let batchInstall = null;
+  const existingNpm = running.find(({ run }) => run.startsWith(ENV.PACKAGE_MANAGER + ' '));
   for (const { id, run, engine, env } of batch) {
     if (engine !== 'shell' || !run.startsWith(ENV.PACKAGE_MANAGER + ' ')) continue;
     const args = run.slice(ENV.PACKAGE_MANAGER.length + 1).split(' ');
     if (args[0] === 'init' && args[1] === '-y' && args.length === 2) {
-      const existingNpm = running.find(({ run }) => run.startsWith(ENV.PACKAGE_MANAGER + ' '));
       if (existingNpm) {
         completionMap[id] = existingNpm.id;
         continue;
       }
     }
-    if (args[0] === INSTALL) {
+    else if (!existingNpm && args[0] === INSTALL) {
       const install = parseInstall(args.slice(1));
-      if (!install) return;
+      if (!install) continue;
       if (running.find(({ run }) => run.startsWith(ENV.PACKAGE_MANAGER + ' ')) ||
           batchInstall && batchInstall.isDev !== install.isDev) {
         defer.push(id);
@@ -85,8 +91,7 @@ Chomp.registerBatcher('npm', function (batch, running) {
       }
       if (!batchInstall) {
         batchInstall = { isDev: install.isDev, env, engine, ids: [id], packages: install.packages };
-      }
-      else {
+      } else {
         for (const key of Object.keys(env)) {
           if (!Object.hasOwnProperty.call(batchInstall.env, key))
             batchInstall.env[key] = env[key];
@@ -97,6 +102,13 @@ Chomp.registerBatcher('npm', function (batch, running) {
             batchInstall.packages.push(pkg);
         }
       }
+    }
+  }
+  // defer any shell error hints while npm install operations are still running
+  if (batchInstall) {
+    for (const { id, run, engine } of batch) {
+      if (engine !== 'shell' || !run.includes(NPM_MISSING_MESSAGE))
+      defer.push(id);
     }
   }
   const exec = batchInstall ? [{
