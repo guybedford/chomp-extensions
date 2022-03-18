38633dddba2971057cc96f3b46c5845e46878eb4
JayaKrishnaNamburu <namburu1995@gmail.com>
2022-03-18 16:13:34 +0530

    Adding support for plugins in rollup

--- rollup.js
+++ rollup.js
@@ -1,7 +1,7 @@
 Chomp.addExtension('chomp@0.1:npm');
 
 Chomp.registerTemplate('rollup', function (task) {
-  const { outdir = 'dist', entries, sourceMaps = true, autoInstall } = task.templateOptions;
+  const { outdir = 'dist', entries, sourceMaps = true, autoInstall, plugins = [] } = task.templateOptions;
   if (!Array.isArray(entries) || !entries.every(entry => typeof entry === 'string'))
     throw new Error("'entries' must be an array of string module input paths for the Rollup plugin.");
   const targets = entries.map(entry => outdir + '/' + entry.split('/').pop());
@@ -9,12 +9,19 @@ Chomp.registerTemplate('rollup', function (task) {
     name: task.name,
     deps: [...task.deps, ...ENV.CHOMP_EJECT ? ['npm:install'] : ['node_modules/rollup']],
     targets: [...new Set([...task.targets, ...targets])],
-    run: `rollup ${entries.join(' ')} -d ${outdir}${sourceMaps ? ' -m' : ''}`
+    run: `rollup ${
+      plugins.length > 0
+        ? `${plugins.reduce((acc, plugin) => {
+            acc = acc + `--plugin ${plugin} `;
+            return acc;
+          }, "")}`
+        : ""
+    } ${entries.join(' ')} -d ${outdir}${sourceMaps ? ' -m' : ''}`
   }, ...ENV.CHOMP_EJECT ? [] : [{
     template: 'npm',
     templateOptions: {
       autoInstall,
-      packages: ['rollup@2'],
+      packages: ['rollup@2', ...plugins],
       dev: true
     }
   }]];
