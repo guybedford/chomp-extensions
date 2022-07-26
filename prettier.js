64da1c377ffb96d140c425709b81ce6c49921db3
Zach Smith <zachsmith.sa@gmail.com>
2022-07-26 09:24:12 +0200

    Updated prettier extension to use chomp watch-invalidation, and updated the template to use prettier caching options. Resolves (#18)

--- prettier.js
+++ prettier.js
@@ -1,29 +1,63 @@
-Chomp.addExtension('chomp@0.1:prettier');
+Chomp.addExtension('chomp@0.1:npm')
 
-Chomp.registerTemplate('prettier', function ({ name, targets, deps, env, templateOptions: { files = '.', check = false, write = true, config = null, noErrorOnUnmatchedPattern = false, autoInstall, ...invalid } }) {
-  if (Object.keys(invalid).length)
-    throw new Error(`Invalid prettier template option "${Object.keys(invalid)[0]}", expected one of "files", "check", "write", "config", "no-error-on-unmatched-pattern" or "auto-install".`);
-  return [{
+Chomp.registerTemplate(
+  'prettier',
+  function ({
     name,
     targets,
-    deps: [...deps, ...ENV.CHOMP_EJECT ? ['npm:install'] : ['node_modules/prettier']],
-    invalidation: 'always',
+    deps,
     env,
-    run: `prettier ${files} ${
-        check ? ' --check' : ''
-      }${
-        write ? ' --write' : ''
-      }${
-        config ? ` --config ${config}` : ''
-      }${
-        noErrorOnUnmatchedPattern ? ' --no-error-on-unmatched-pattern' : ''
-      }`
-  }, ...ENV.CHOMP_EJECT ? [] : [{
-    template: 'npm',
     templateOptions: {
+      files = false,
+      check = false,
+      write = true,
+      config = null,
+      loglevel = false,
+      noErrorOnUnmatchedPattern = false,
+      ignorePath = false,
       autoInstall,
-      packages: ['prettier'],
-      dev: true
-    }
-  }]];
-});
+      ...invalid
+    },
+  }) {
+    if (Object.keys(invalid).length)
+      throw new Error(
+        `Invalid prettier template option "${
+          Object.keys(invalid)[0]
+        }", expected one of "files", "check", "write", "config", "no-error-on-unmatched-pattern" or "auto-install".`
+      )
+    return [
+      {
+        name,
+        targets,
+        deps: [...deps, ...(ENV.CHOMP_EJECT ? ['npm:install'] : ['node_modules/prettier'])],
+        watchInvalidation: 'skip-running',
+        invalidation: 'always',
+        env,
+        run: `prettier --cache --cache-strategy metadata --loglevel ${loglevel || 'log'}${
+          ignorePath ? ` --ignore-path ${ignorePath}` : ''
+        }${config ? ` --config ${config}` : ''}${
+          noErrorOnUnmatchedPattern ? ' --no-error-on-unmatched-pattern' : ''
+        }${check ? ' --check' : ''}${write ? ' --write' : ''} ${
+          files
+            ? files
+                .split(' ')
+                .map(ex => `"${ex}"`)
+                .join(' ')
+            : '.'
+        }`,
+      },
+      ...(ENV.CHOMP_EJECT
+        ? []
+        : [
+            {
+              template: 'npm',
+              templateOptions: {
+                autoInstall,
+                packages: ['prettier'],
+                dev: true,
+              },
+            },
+          ]),
+    ]
+  }
+)
